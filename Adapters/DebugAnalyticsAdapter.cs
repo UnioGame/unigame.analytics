51a8a4972d57d8d11633f9479959d7ae2d5bb425
Mefodei <mefodei.link@gmail.com>
2026-05-24 00:32:00 +0700

    - update debug analytics logs

--- Adapters/DebugAnalyticsAdapter.cs
+++ Adapters/DebugAnalyticsAdapter.cs
@@ -1,18 +1,23 @@
 ﻿namespace UniGame.Runtime.Analytics.Adapters
 {
     using System;
-    using System.Linq;
     using Cysharp.Threading.Tasks;
     using Interfaces;
+#if GAME_DEBUG
+    using System.Text;
     using UniCore.Runtime.ProfilerTools;
     using UnityEngine;
+#endif
 
     [Serializable]
     public class DebugAnalyticsAdapter : IAnalyticsAdapter
     {
+#if GAME_DEBUG
+        private static readonly StringBuilder _builder = new(256);
+#endif
+
         public void Dispose()
         {
-            
         }
 
         public UniTask InitializeAsync()
@@ -23,8 +28,20 @@
         public void TrackEvent(IAnalyticsMessage message)
         {
 #if GAME_DEBUG
-            var payload = string.Join(", ", message.Parameters.Select(static x => $"{x.Key}={x.Value}"));
-            GameLog.Log($"[Analytics] {message.Name} | {payload}",Color.chocolate);
+            var builder = _builder;
+            builder.Clear();
+            builder.Append("[Analytics] ").Append(message.Name).Append(" | ");
+
+            var first = true;
+            foreach (var pair in message.Parameters)
+            {
+                if (!first)
+                    builder.Append(", ");
+                builder.Append(pair.Key).Append('=').Append(pair.Value);
+                first = false;
+            }
+
+            GameLog.Log(builder.ToString(), Color.chocolate);
 #endif
         }
     }
